eaa0a7c9c277afe000ac1dc60176e81ba55772f1
amtkxa <scarlet0211@outlook.jp>
2018-03-03 21:01:53 +0900

    コメントを追加 バリデーション及びコールバックの実装を追加

--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -1,13 +1,31 @@
 class Book < ApplicationRecord
+  # スコープ
   scope :constly, -> { where("price > ?", 3000)}
   scope :written_about, ->(theme) { where("name like ?", "%#{theme}%")}
 
+  # リレーション
   belongs_to :publisher
-
   has_many :book_authors
   has_many :authers, through: :book_authors
 
+  # バリデーション
   validates :name, presence: true
   validates :name, length: { maximum: 15 }
   validates :price, numericality: { greater_than_or_equal_to: 0}
+  validates do |book|
+    if book.name.include?("exercise")
+      book.errors[name] << "I don't like exercise."
+    end
+  end
+
+  # コールバック
+  before_validation do |book|
+    book.name = book.name.gsub(/Cat/) do |matched|
+      "lovery #{matched}"
+    end
+  end
+  after_destroy do |book|
+    Rails.logger.info "Book is deleted: #{book.attributes.inspect}"
+  end
+
 end
