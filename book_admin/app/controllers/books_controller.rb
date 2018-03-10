3983ba61ab6c12684aa62038e2513044e6bf76e4
amtkxa <scarlet0211@outlook.jp>
2018-03-10 14:22:10 +0900

    BooksControllerにsetterを追加

--- book_admin/app/controllers/books_controller.rb
+++ book_admin/app/controllers/books_controller.rb
@@ -1,10 +1,17 @@
 class BooksController < ApplicationController
-  def show
-    @book = Book.find(params[:id])
-    respond_to do |format|
-      format.html
-      format.csv
-      format.json
-    end
+  # 前処理
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
+  @book = Book.find(params[:id])
+  respond_to do |format|
+    format.html
+    format.csv
+    format.json
+  end
+
+  # Bookモデルのインスタンスを変数に代入する
+  private
+  def set_books
+    @book = Book.find(param[:id])
   end
 end
