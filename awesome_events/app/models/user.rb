449d5dad1f6901844b467662ebc8e034ad5c6396
amtkxa <scarlet0211@outlook.jp>
2018-03-16 00:44:44 +0900

    Twitterアプリケーション認証機能を実装

--- awesome_events/app/models/user.rb
+++ awesome_events/app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ActionController::Base
+class User < ActiveRecord::Base
   def self.find_or_create_from_auth_hash(auth_hash)
     provider = auth_hash[:provider]
     uid = auth_hash[:uid]
--- awesome_events/db/schema.rb
+++ awesome_events/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315145509) do
+ActiveRecord::Schema.define(version: 20180315153710) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.string "nickname"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "usrs", force: :cascade do |t|
     t.string "provider"
