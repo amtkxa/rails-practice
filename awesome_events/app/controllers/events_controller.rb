558dce33eca343a7290526bd2c9585d1e5ebdf33
amtkxa <scarlet0211@outlook.jp>
2018-03-17 21:36:04 +0900

    Event関連のコントローラ等を修正

--- awesome_events/app/controllers/events_controller.rb
+++ awesome_events/app/controllers/events_controller.rb
@@ -1,2 +1,24 @@
 class EventsController < ApplicationController
+  before_action :authenticate
+
+  def new
+    @event = current_user.created_events.build
+  end
+
+  def create
+    @event = create_user.created_events.build(event_params)
+    if @event.save
+      redirect_to @event, notice: '作成しました'
+    else
+      renser :new
+    end
+  end
+
+  private
+
+  def events_@arams
+    params.require(:event).permit(
+      :name, :place, :content, :start_time, :end_time
+    )
+  end
 end
